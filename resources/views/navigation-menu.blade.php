9aa1b85bd5c9b27ab2c34e939ec8acce8dc49636
developer-chetaru <developer@chetaru.com>
2026-01-27 11:42:58 +0000

    sunit

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -191,7 +191,7 @@
 
                 <!-- Change Password -->
                 <a href="{{ route('password.change') }}"
-                   class="flex items-center px-1 py-1 sm:px-4 sm:py-2 hover:bg-gray-100 space-x-2">
+                   class="flex items-center px-4 py-1 sm:px-4 sm:py-2 hover:bg-gray-100 space-x-2">
                     <svg class="w-4 h-4 text-gray-600" fill="none" stroke="currentColor"
                          stroke-width="1.5" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round"
